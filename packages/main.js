94b3b501ffb8abb85be005723d34c976d09d5b4a
shaoyishou <shaoyishou@gmail.com>
2018-01-02 11:37:29 +0800

    add some validator method

--- packages/main.js
+++ packages/main.js
@@ -2,10 +2,12 @@ import toDou from './toDou';
 import setTitle from './setTitle';
 import cookie from './cookie';
 import query from './queryUrl';
+import validator from './validator';
 
 export default {
   toDou,
   setTitle,
   cookie,
-  query
+  query,
+  validator
 }
